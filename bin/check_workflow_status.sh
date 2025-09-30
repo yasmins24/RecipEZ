66f0730164f6be43ab7ee73dcdb13588750ef10c
yasmins24 <yasmin.sayeed@gmail.com>
2025-09-30 09:41:41 -0700

    Ok

--- bin/check_workflow_status.sh
+++ bin/check_workflow_status.sh
@@ -7,16 +7,16 @@ repo=$2
 response=$(curl -f -H "Authorization: token $token" \
         https://api.github.com/repos/yasmins24/$repo/actions/workflows/build.yml/runs?per_page=1 | jq -r '.workflow_runs[0]')
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
+  status=$(echo $response | jq -r '.status')
+  conclusion=$(echo $response | jq -r '.conclusion')
+
+  echo "$repo-conclusion=$conclusion" >> "$GITHUB_OUTPUT"
+  echo "$repo-status=$status" >> "$GITHUB_OUTPUT"
+
+  echo $status
+  echo $conclusion
+fi
+
   echo "Getting workflow status for $repo failed."
   exit 1
-fi
-        
-status=$(echo $response | jq -r '.status')
-conclusion=$(echo $response | jq -r '.conclusion')
-
-echo "$repo-conclusion=$conclusion" >> "$GITHUB_OUTPUT"
-echo "$repo-status=$status" >> "$GITHUB_OUTPUT"
-
-echo $status
-echo $conclusion

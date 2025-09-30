697b68a9210917c49d13a1edc779eb078984288e
yasmins24 <yasmin.sayeed@gmail.com>
2025-09-29 21:15:34 -0700

    Updates

--- bin/check_workflow_status.sh
+++ bin/check_workflow_status.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-response=$(curl -H "Authorization: token $1" \
-          https://api.github.com/repos/yasmins24/PickMyMeals/actions/workflows/build.yml/runs?per_page=1 | jq -r '.workflow_runs[0]')
-          
+# Grab auth token and repository name
+token=$1
+repo=$2
+
+response=$(curl -H "Authorization: token $token" \
+        https://api.github.com/repos/yasmins24/$repo/actions/workflows/build.yml/runs?per_page=1 | jq -r '.workflow_runs[0]')
+        
 status=$(echo $response | jq -r '.status')
 conclusion=$(echo $response | jq -r '.conclusion')
 
-echo "conclusion=$conclusion" >> "$GITHUB_OUTPUT"
-echo "status=$status" >> "$GITHUB_OUTPUT"
+echo "$repo-conclusion=$conclusion" >> "$GITHUB_OUTPUT"
+echo "$repo-status=$status" >> "$GITHUB_OUTPUT"

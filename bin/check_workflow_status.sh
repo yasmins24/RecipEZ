23e0344e88153a6385cd964a7964dffea2ff1b48
yasmins24 <yasmin.sayeed@gmail.com>
2025-09-30 09:04:58 -0700

    Check error handling

--- bin/check_workflow_status.sh
+++ bin/check_workflow_status.sh
@@ -6,6 +6,10 @@ repo=$2
 
 response=$(curl -H "Authorization: token $token" \
         https://api.github.com/repos/yasmins24/$repo/actions/workflows/build.yml/runs?per_page=1 | jq -r '.workflow_runs[0]')
+
+if [ $? -ne 0 ]; then
+  echo "Getting workflow status for $repo failed with an exit code of: $?"
+fi
         
 status=$(echo $response | jq -r '.status')
 conclusion=$(echo $response | jq -r '.conclusion')
